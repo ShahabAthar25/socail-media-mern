5fabca8352a266756837a5b5f640bf059538f6b6
ShahabAthar25 <shahabathar25@gmail.com>
2021-11-26 13:31:15 +0500

    Add User model

---
+++ backend/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 60,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 60,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    coverPic: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
