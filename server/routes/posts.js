71b64c124987eb9303e343bc250917be977f09c7
ShahabAthar25 <shahabathar25@gmail.com>
2021-12-13 15:57:43 +0500

    Initail commit

--- server/routes/posts.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const controller = require("../controllers/posts");
-
-router.get("/", controller.getPosts);
-router.post("/", controller.createPost);
-
-module.exports = router;
