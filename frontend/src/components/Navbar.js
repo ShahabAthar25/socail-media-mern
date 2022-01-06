a9586957796e53ab5207e09de8ef534ffaff149d
ShahabAthar25 <shahabathar25@gmail.com>
2022-01-06 22:38:18 +0500

    Made navbar responsive

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,6 +8,11 @@ import {
 } from "@heroicons/react/outline";
 
 import { ReactComponent as Logo } from "../images/logo.svg";
+import { ReactComponent as Friends } from "../images/friends.svg";
+import { ReactComponent as Gaming } from "../images/gaming.svg";
+import { ReactComponent as Home } from "../images/home.svg";
+import { ReactComponent as MarketPlace } from "../images/marketPlace.svg";
+import { ReactComponent as Watch } from "../images/watch.svg";
 
 export default function Navbar() {
   return (
@@ -15,12 +20,37 @@ export default function Navbar() {
       <div className="flex">
         <div className="space-x-2 flex items-center">
           <Logo className="" />
-          <div className="text-gray-400 bg-[#F0F2F5] rounded-full p-2">
+          <div className="text-gray-400 bg-[#F0F2F5] rounded-full p-2 md:hidden">
             <SearchIcon className="h-6" />
           </div>
         </div>
-        <div className="hover:bg-[#F0F2F5] mx-5 p-2 rounded-lg">
+        <form className="hidden md:flex items-center bg-[#F0F2F5] rounded-full p-2 mx-2">
+          <SearchIcon className="h-6 text-gray-400" />
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            className="bg-transparent focus:outline-none"
+          />
+        </form>
+        <div className="hover:bg-[#F0F2F5] mx-5 p-2 rounded-lg sm:hidden">
           <MenuIcon className="h-8" />
+        </div>
+      </div>
+      <div className="hidden sm:flex items-center justify-center flex-grow max-w-md">
+        <div className="py-3 flex-grow flex flex-col items-center justify-center rounded-lg cursor-pointer">
+          <Home />
+        </div>
+        <div className="hover:bg-[#F0F2F5] py-3 flex-grow flex items-center justify-center rounded-lg cursor-pointer">
+          <Friends />
+        </div>
+        <div className="hover:bg-[#F0F2F5] py-3 flex-grow flex items-center justify-center rounded-lg cursor-pointer">
+          <Watch />
+        </div>
+        <div className="hover:bg-[#F0F2F5] py-3 flex-grow flex items-center justify-center rounded-lg cursor-pointer">
+          <MarketPlace />
+        </div>
+        <div className="hover:bg-[#F0F2F5] py-3 flex-grow flex items-center justify-center rounded-lg cursor-pointer">
+          <Gaming />
         </div>
       </div>
       <div className="flex space-x-2">
